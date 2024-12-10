605d65e1ea7e4591ef0f47ecd62dffe72aff13f9
Avtaev Miroslav <135225762+Miroslifea@users.noreply.github.com>
2024-12-10 22:40:07 +0300

    Update GeometricFunctions.py

--- geometric_lib/GeometricFunctions.py
+++ geometric_lib/GeometricFunctions.py
@@ -14,7 +14,7 @@ def circlePerimeter(r):
         raise TypeError("Радиус должен быть числом.")
     if r < 0:
         raise ValueError("Радиус не может быть отрицательным.")
-    return math.pi * r
+    return math.pi * r * 2
 
 def rectangleArea(a, b):
     '''Принимает 2 числа, a и b. a - первая сторона прямоугольника, b - вторая сторона прямоугольника. Возвращает площадь прямоугольника.'''
